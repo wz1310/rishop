a55737c50dbfbffcb42d1dd205e002f1142aae9e
wz1310 <a002041zano@gmail.com>
2025-12-01 20:34:34 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,23 @@ import Category from './components/Category'
 import Header from './components/Header'
 import Submenu from './components/Submenu'
 
+const CardSection = ({ children }) => {
+  return (
+    <div className="bg-white shadow-xl w-[calc(100%-4px)] md:w-full max-w-5xl mt-6 mx-auto rounded-xl">
+      {children}
+    </div>
+  );
+};
+
 const App = () => {
   return (
-    <main className="overflow-x-hidden antialiased text-neutral-800 pt-27">
-      <Header />
-      <Submenu />
-      <Category/>
+    <main className="overflow-x-hidden antialiased text-neutral-800 pt-27 bg-gray-100 min-h-screen flex flex-col items-center">
+      <CardSection><Header /></CardSection>
+      <CardSection><Submenu /></CardSection>
+      <CardSection><Category /></CardSection>
+
+      {/* contoh jika tambah content baru nanti */}
+      {/* <CardSection><KomponenBaru /></CardSection> */}
     </main>
   )
 }
